47acbac4f3cdb3ba14846bbb01bd793ada2bedf3
edelcastillo <edelcastillo@coresecurity.com>
2018-03-16 10:07:34 -0300

    Agragada la funcion procesar_linea

--- Python/script_test.py
+++ Python/script_test.py
@@ -16,26 +16,29 @@ def actualizar_archivo(path, dic) :
 	tam_linea = archivo.tell()
 	archivo.seek(0,0)
 	for linea in archivo.readlines() :
-		linea = linea[:-1]
-		lista = linea.split("|")
-		print lista
-		aumento = dic.get(lista[1])
-		print aumento
-		if aumento == None :
-			continue
-		aumento = float(aumento) / (100) + 1
-		print aumento
-		para_atras = tam_linea * -1
-		print para_atras
-		archivo.seek(para_atras,1)
-		archivo.write(lista[0]+"|"+lista[1]+"|")
-		print lista[-1]
-		sueldo_actual = float(lista[-1]) * aumento
-		print sueldo_actual
-		archivo.write(str(sueldo_actual)+"\n")
-		archivo.seek(0,1)
+		procesar_linea(archivo, tam_linea, linea, dic)
 
-path_lista = "C:/Users/folivera/Documents/Python/lista_empleados.txt"
-path_aumentos = "C:/Users/folivera/Documents/Python/empleados_aumento.txt"
+def procesar_linea(archivo, tam_linea, linea, dic):
+	linea = linea[:-1]
+	lista = linea.split("|")
+	print lista
+	aumento = dic.get(lista[1])
+	print aumento
+	if aumento == None :
+		return
+	aumento = float(aumento) / (100) + 1
+	print aumento
+	para_atras = tam_linea * -1
+	print para_atras
+	archivo.seek(para_atras,1)
+	archivo.write(lista[0]+"|"+lista[1]+"|")
+	print lista[-1]
+	sueldo_actual = float(lista[-1]) * aumento
+	print sueldo_actual
+	archivo.write(str(sueldo_actual)+"\n")
+	archivo.seek(0,1)
+
+path_lista = "lista_empleados.txt"
+path_aumentos = "empleados_aumento.txt"
 dic_aumentos = armar_dic(path_aumentos)
 actualizar_archivo(path_lista, dic_aumentos)
